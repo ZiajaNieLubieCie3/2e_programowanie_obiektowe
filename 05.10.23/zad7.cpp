fc23bc2fe8228c73ce1410634b3a0113445a5061
ZiajaNieLubieCie3 <145669951+ZiajaNieLubieCie3@users.noreply.github.com>
2023-10-05 13:12:47 +0200

    zad7a.cpp

--- 05.10.23/zad7.cpp
+++ 05.10.23/zad7.cpp
@@ -13,12 +13,12 @@ cout << "Wprowadz druga liczbe: ";
 cin >> y;
 cout << "Wprowadz trzecia liczbe: ";
 cin >> z;
-if(x > y && y > z)
-cout << x << "jest najwieksze";
-else if ( y > x && y > z)
-cout << y << "jest najwieksze";
-else if (z > y && z > x)
-cout << z << "jest najwieksze";
+if(x < y && y < z)
+cout << x << "jest najmniejsze";
+else if ( y < x && y < z)
+cout << y << "jest najmniejsze";
+else if (z < y && z < x)
+cout << z << "jest najmniejsze";
 else
 cout << "liczby sa te same";
 }
