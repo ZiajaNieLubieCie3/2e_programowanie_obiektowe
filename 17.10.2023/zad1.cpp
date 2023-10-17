42ad6ed40070b582b91a94b7cace430dab4ea6d2
ZiajaNieLubieCie3 <145669951+ZiajaNieLubieCie3@users.noreply.github.com>
2023-10-17 14:52:20 +0200

    Add files via upload

---
+++ 17.10.2023/zad1.cpp
@@ -0,0 +1,60 @@
+﻿/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+
+{
+    string login = "login1";
+    string haslo = "haslo1";
+    int bledy = 3;
+
+    do {
+        cout << "Wprowadz login";
+        cin >> login;
+        if (login != "login1")
+        {
+            cout << "Bledny login\n";
+            bledy--;
+
+        }
+        else
+        {
+            do {
+                cout << "Wprowadz haslo: ";
+                cin >> haslo;
+                    if (haslo != "haslo1")
+                    {
+                        cout << "bledne haslo\n";
+                            bledy--;
+                    }
+                    else
+                    {
+                        cout << "zalogowano";
+                        goto exit;
+
+                    }
+            } while (bledy > 0);
+        }
+        if (bledy == 0)
+        {
+            cout << "Zablokowano możliwość zalogowania";
+        }
+
+
+
+
+    } while (bledy > 0);
+
+exit:
+    return 0;
+}
---
+++ 17.10.2023/zad2.cpp
@@ -0,0 +1,37 @@
+﻿// zad2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y = 1;
+    cout << "Wprowadz liczbe parzysta: ( 0 konczy dzialanie) ";
+    cin >> x;
+    if (x == 0)
+    {
+        cout << "Nie wprowadzono zadnej liczby";
+        goto exit;
+    }
+    do {
+        if (x % 2 != 0)
+        {
+            do {
+                cout << "Wprowadzono liczbe nieparzysta, wprowadz parzysta: ";
+                cin >> x;
+            } while (x % 2 != 0);
+        }
+        if (x == 0)
+        {
+            cout << "Iloczyn wprowadzonych liczb jest rowny: " << y;
+            goto exit;
+        }
+        y = y * x;
+        cout << "Wprowadz liczbe parzysta: (liczba 0 kończy zadawanie pytań o liczbę) ";
+        cin >> x;
+    } while (x % 2 == 0);
+exit:
+    return 0;
+}
