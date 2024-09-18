319b10aca4dfe0bde01a707a3d646bed88d2a977
ZiajaNieLubieCie3 <145669951+ZiajaNieLubieCie3@users.noreply.github.com>
2024-09-18 13:45:12 +0200

    Add files via upload

---
+++ 18.09.24/szplagablaga.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace zacharzgrubas
+{
+    class program
+
+    {
+        public static void Main(string[] args)
+        {
+            float PI = float.Pi;
+            string dane = "Nikodem Szałas";
+
+            int x = 10;
+            float y = (float)x;
+
+
+            double a = 10.78493;
+                int b = Convert.ToInt32(a);
+
+            Console.WriteLine(" Podaj napis ");
+            string napis = Console.ReadLine();
+            Console.WriteLine(" Twoj napis: " + napis);
+
+        }
+    }
+}
